cf6e5f9c8db24aad5f8395b350a8c7c748615ff9
anushanantha <anusha.ananthakumar@gmail.com>
2021-01-31 12:45:56 +0100

    Updating index js to support the website

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ function formatDate(currentDate) {
   let hours = currentDate.getHours();
   let minutes = currentDate.getMinutes();
   let year = currentDate.getFullYear();
-  let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tueday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
   let day = days[currentDate.getDay()];
   let months = [
     "Jan",
@@ -23,10 +31,10 @@ function formatDate(currentDate) {
   minutes = formatMinutes(minutes);
   return ` ${day},  ${hours}:${minutes} <br/>${month} ${date}, ${year}`;
 }
-function  formatMinutes(minutes){
+function formatMinutes(minutes) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
-  } 
+  }
   return minutes;
 }
 let formattedDate = formatDate(new Date());
@@ -36,14 +44,14 @@ dateAndTime.innerHTML = formattedDate;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", updateTemperature);
 
-function returnCelsius(){
+function returnCelsius() {
   let h1 = document.querySelector("h1");
   h1.innerHTML = "0 °C";
 }
 
-function returnFahrenheit(){
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = "32 °F";
+function returnFahrenheit() {
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = "32 °F";
 }
 
 let anchorCelsius = document.querySelector("#celsius-click");
@@ -54,18 +62,17 @@ anchorFahrenheit.addEventListener("click", returnFahrenheit);
 
 // adding the  real-time weather update feature
 
-function updateTemperature(){
+function updateTemperature(event) {
+  event.preventDefault();
   let enterCity = document.querySelector("#search-text-input");
   let h2 = document.querySelector("h2");
   h2.innerHTML = `${enterCity.value}`;
   h2.innerHTML = h2.innerHTML.toUpperCase();
-  console.log(enterCity);
-  console.log(h2);
-  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${enterCity.value} &appid=667b283f74e42ac5a41950daddbddfc8&units=metric`;
-axios.get(apiUrl).then(showTemperature);
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${enterCity.value} &appid=667b283f74e42ac5a41950daddbddfc8&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
-  function showTemperature(response) {
-      console.log(response);
+function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${temperature}˚C`;
@@ -73,28 +80,25 @@ axios.get(apiUrl).then(showTemperature);
 
 //Bonus - display current location and temperature
 
-function getTemperatureUsingLatLong(position){
-let lat = position.coords.latitude;
-let long = position.coords.longitude;
-let reverseLookupApiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${long}`;
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=667b283f74e42ac5a41950daddbddfc8&units=metric`;
- axios.get(apiUrl).then(showTemperature);
+function getTemperatureUsingLatLong(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let reverseLookupApiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${long}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=667b283f74e42ac5a41950daddbddfc8&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
   axios.get(reverseLookupApiUrl).then(displayCityName);
 }
 
-function displayCityName(response){
+function displayCityName(response) {
   let cityName = response.data.address.city;
- let h2 = document.querySelector("h2");
+  let h2 = document.querySelector("h2");
   h2.innerHTML = `${cityName}`;
   h2.innerHTML = h2.innerHTML.toUpperCase();
 }
 
 function showCurrentPosition() {
-navigator.geolocation.getCurrentPosition(getTemperatureUsingLatLong);
+  navigator.geolocation.getCurrentPosition(getTemperatureUsingLatLong);
 }
 
 let currentLocation = document.querySelector("#current-location");
 currentLocation.addEventListener("click", showCurrentPosition);
-
-
- 
